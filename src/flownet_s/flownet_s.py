744a2e448ff20b2c6b8b1c1ea7aa9950a3ef2f39
Shrinath Deshpande <shrinath.deshpande@stonybrook.edu>
2017-12-16 18:14:03 -0500

    Changed output of the model to return Encoded features of Optical Flow

--- src/flownet_s/flownet_s.py
+++ src/flownet_s/flownet_s.py
@@ -7,23 +7,16 @@ slim = tf.contrib.slim
 
 
 class FlowNetS(Net):
-
-    def __init__(self, mode=Mode.TRAIN, debug=False):
+    def __init__(self, mode=Mode.TRAIN, debug=False, reuse=None):
         super(FlowNetS, self).__init__(mode=mode, debug=debug)
+        self.reuse = reuse
 
     def model(self, inputs, training_schedule, trainable=True):
+        """ Returns Encoder output of FlowNetS CNN """
         _, height, width, _ = inputs['input_a'].shape.as_list()
         stacked = False
-        with tf.variable_scope('FlowNetS'):
-            if 'warped' in inputs and 'flow' in inputs and 'brightness_error' in inputs:
-                stacked = True
-                concat_inputs = tf.concat([inputs['input_a'],
-                                           inputs['input_b'],
-                                           inputs['warped'],
-                                           inputs['flow'],
-                                           inputs['brightness_error']], axis=3)
-            else:
-                concat_inputs = tf.concat([inputs['input_a'], inputs['input_b']], axis=3)
+        with tf.variable_scope('FlowNetS', reuse=self.reuse):
+            concat_inputs = tf.concat([inputs['input_a'], inputs['input_b']], axis=3)
             with slim.arg_scope([slim.conv2d, slim.conv2d_transpose],
                                 # Only backprop this network if trainable
                                 trainable=trainable,
@@ -48,76 +41,8 @@ class FlowNetS(Net):
                         conv5_1 = slim.conv2d(pad(conv5), scope='conv5_1')
                     conv6 = slim.conv2d(pad(conv5_1), 1024, 3, stride=2, scope='conv6')
                     conv6_1 = slim.conv2d(pad(conv6), 1024, 3, scope='conv6_1')
-
-                    """ START: Refinement Network """
-                    with slim.arg_scope([slim.conv2d_transpose], biases_initializer=None):
-                        predict_flow6 = slim.conv2d(pad(conv6_1), 2, 3,
-                                                    scope='predict_flow6',
-                                                    activation_fn=None)
-                        deconv5 = antipad(slim.conv2d_transpose(conv6_1, 512, 4,
-                                                                stride=2,
-                                                                scope='deconv5'))
-                        upsample_flow6to5 = antipad(slim.conv2d_transpose(predict_flow6, 2, 4,
-                                                                          stride=2,
-                                                                          scope='upsample_flow6to5',
-                                                                          activation_fn=None))
-                        concat5 = tf.concat([conv5_1, deconv5, upsample_flow6to5], axis=3)
-
-                        predict_flow5 = slim.conv2d(pad(concat5), 2, 3,
-                                                    scope='predict_flow5',
-                                                    activation_fn=None)
-                        deconv4 = antipad(slim.conv2d_transpose(concat5, 256, 4,
-                                                                stride=2,
-                                                                scope='deconv4'))
-                        upsample_flow5to4 = antipad(slim.conv2d_transpose(predict_flow5, 2, 4,
-                                                                          stride=2,
-                                                                          scope='upsample_flow5to4',
-                                                                          activation_fn=None))
-                        concat4 = tf.concat([conv4_1, deconv4, upsample_flow5to4], axis=3)
-
-                        predict_flow4 = slim.conv2d(pad(concat4), 2, 3,
-                                                    scope='predict_flow4',
-                                                    activation_fn=None)
-                        deconv3 = antipad(slim.conv2d_transpose(concat4, 128, 4,
-                                                                stride=2,
-                                                                scope='deconv3'))
-                        upsample_flow4to3 = antipad(slim.conv2d_transpose(predict_flow4, 2, 4,
-                                                                          stride=2,
-                                                                          scope='upsample_flow4to3',
-                                                                          activation_fn=None))
-                        concat3 = tf.concat([conv3_1, deconv3, upsample_flow4to3], axis=3)
-
-                        predict_flow3 = slim.conv2d(pad(concat3), 2, 3,
-                                                    scope='predict_flow3',
-                                                    activation_fn=None)
-                        deconv2 = antipad(slim.conv2d_transpose(concat3, 64, 4,
-                                                                stride=2,
-                                                                scope='deconv2'))
-                        upsample_flow3to2 = antipad(slim.conv2d_transpose(predict_flow3, 2, 4,
-                                                                          stride=2,
-                                                                          scope='upsample_flow3to2',
-                                                                          activation_fn=None))
-                        concat2 = tf.concat([conv_2, deconv2, upsample_flow3to2], axis=3)
-
-                        predict_flow2 = slim.conv2d(pad(concat2), 2, 3,
-                                                    scope='predict_flow2',
-                                                    activation_fn=None)
-                    """ END: Refinement Network """
-
-                    flow = predict_flow2 * 20.0
-                    # TODO: Look at Accum (train) or Resample (deploy) to see if we need to do something different
-                    flow = tf.image.resize_bilinear(flow,
-                                                    tf.stack([height, width]),
-                                                    align_corners=True)
-
-                    return {
-                        'predict_flow6': predict_flow6,
-                        'predict_flow5': predict_flow5,
-                        'predict_flow4': predict_flow4,
-                        'predict_flow3': predict_flow3,
-                        'predict_flow2': predict_flow2,
-                        'flow': flow,
-                    }
+                    print conv6_1
+                    return conv6_1
 
     def loss(self, flow, predictions):
         flow = flow * 0.05
